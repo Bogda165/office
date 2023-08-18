e565b1f188d088d56015a8ce7ea7ca81be4e3aac
Bogda165 <koval.b@icloud.com>
2023-08-18 18:53:34 +0200

    Initial Commit

---
+++ office/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  office
+//
+//  Created by Богдан Коваль on 18.08.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
